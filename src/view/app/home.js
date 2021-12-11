5a66b50b84c5d60dba21e09bc669115a33af884b
sujith-sk-07 <venuvikaash006@gmail.com>
2021-12-11 23:39:18 +0530

    changes

--- src/view/app/home.js
+++ src/view/app/home.js
@@ -4,16 +4,31 @@ import BitCoin from '../../assets/img/icons8-bitcoin.svg'
 
 function HomePage() {
     return (
-        <div className='navbar'>
-            <div className='logoSize'>
-                <img src={BitCoin} alt='' className='m-2' />
+        <div>
+            <div className='navbar navbar-expand-lg backGround'>
+            <a href className='navbar-brand'>
+                <img src={BitCoin} alt='' className='m-2 logoSize' />
+            </a>
+            <div className='collapse navbar-collapse' id='navbar-nav'>
+                <ul className='navbar-nav'>
+                    <li className='nav-item mx-2 fontColor'>Home</li>
+                    <li className='nav-item mx-2 fontColor'>About</li>
+                    <li className='nav-item mx-2 fontColor'>Contact</li>
+                    {/* <li className='nav-item'></li> */}
+                </ul>
             </div>
-            <h2 className="m-2">hello Crypto!!</h2>
-            <div className=''>
-                <p className='mx-3 my-2'>home</p>
-                <p className='mx-3 my-2'>bitcoin</p>
-                <p className='mx-3 my-2'>API</p>
-                <p className='mx-3 my-2'>contact</p>
+            </div>
+            <div className="container loginPage">
+            <div className='col-md-12 d-flex justify-content-evenly loginPage'>
+                <div classname="bit-1 col-md-5">
+                    <h2>Hello Crypto !!</h2>
+                </div>
+                <div className='bit-1 card col-md-3'>
+                    <h2>Login</h2>
+                    <input type="email" className='m-3' name='E-mail'/>
+                    <input type="password" className='m-3'/>
+                </div>
+            </div>
             </div>
         </div>
     );
