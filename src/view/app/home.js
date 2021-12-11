6456d8a7dfa46f3fc55442481f7768d245e3021f
sujith-sk-07 <venuvikaash006@gmail.com>
2021-12-11 21:16:40 +0530

    changes

--- src/view/app/home.js
+++ src/view/app/home.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BitCoin from '../../assets/icons8-bitcoin.svg'
+import BitCoin from '../../assets/img/icons8-bitcoin.svg'
 
 function HomePage() {
     return (
-        <div className='bit-1 d-flex justify-content-between'>
+        <div className='navbar'>
             <div className='logoSize'>
                 <img src={BitCoin} alt='' className='m-2' />
             </div>
             <h2 className="m-2">hello Crypto!!</h2>
-            <div className='d-flex justify-content-end'>
+            <div className=''>
                 <p className='mx-3 my-2'>home</p>
                 <p className='mx-3 my-2'>bitcoin</p>
                 <p className='mx-3 my-2'>API</p>
