064210618b91b2edc66dba9dd780629478dbfdaf
sujith-sk-07 <venuvikaash006@gmail.com>
2021-12-11 14:25:35 +0530

    initial commit

---
+++ src/view/app/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div className='bit-1'>
+            <div>
+                <h2 className='justify-content-center'>Hello Crypto !!</h2>
+            </div>
+            <div>
+                <h3>home</h3>
+                <h3>bitcoin</h3>
+                <h3>API</h3>
+                <h3>contact</h3>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
